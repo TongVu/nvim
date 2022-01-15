b273f1c816a71a942faa1e7378eda99460e262f1
Tong Vu <tongvu.244@gmail.com>
2022-01-15 22:18:46 +0700

    add null-ls autoFormat function

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -18,4 +18,15 @@ null_ls.setup({
         formatting.stylua,
         -- diagnostics.flake8
     },
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd([[
+            augroup LspFormatting
+            autocmd! * <buffer>
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+        end
+    end,
 })
