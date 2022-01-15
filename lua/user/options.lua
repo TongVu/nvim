0567b2874f95d976dc28506e92a8834deea89720
Tong Vu <tongvu.244@gmail.com>
2022-01-15 07:54:01 +0700

    set blinkrate

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -48,6 +48,4 @@ opt.guifont = "monospace:h17"               -- the font used in graphical neovim
 opt.shortmess:append "c"
 vim.cmd[[ set whichwrap+=<,>,[,],h,l ]]
 vim.cmd[[ set iskeyword+=- ]]
-cmd[[set guicursor=n:blinkwait100 ]]
-cmd[[set guicursor=n:blinkoff50 ]]
-cmd[[set guicursor=n:blinkon100 ]]
+cmd[[set guicursor=n:blinkwait100-blinkoff50-blinkon100]]
